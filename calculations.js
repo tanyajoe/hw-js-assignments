18a7ce7b9515b302581014cc2ffdb90ddd767360
Pozdeeva.T <tanyajoooe@gmail.com>
2022-10-29 21:44:01 +0300

    correct mistake

--- calculations.js
+++ calculations.js
@@ -3,15 +3,16 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(x, y) {
+  //добавлен параметр y
+  x = x - y;
+  return x;
 }
 
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
+  console.log("x"); //возможно, лишняя строка кода, т. к. все результат выполнения всех функций выводится в консоль отдельно
 }
 
 function getRestAfterDivision(x, y) {
@@ -21,5 +22,5 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2, 4000)); //содержит один лишний параметр
 console.log(getRestAfterDivision(7000, 3));
